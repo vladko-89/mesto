d58019a1df48c49456c484d64a37250c333e3e22
vladko-89 <63239096+vladko-89@users.noreply.github.com>
2020-11-03 16:45:00 +0400

    feat: Форма добавления карты, обработка открытия закрытия

--- js/script.js
+++ js/script.js
@@ -1,16 +1,86 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// --- ФУНКЦИЯ плавного появления модального окна  ---
+const modalWindow = document.querySelector('.popup');
+let modalProfile = document.querySelector('.popup__body');
+
+function popupSlice() {
+  modalProfile.classList.toggle('popup__body_slice');
+}
+
+
 // --- ФУНКЦИЯ Открытие формы ---
-let modalWindow = document.querySelector('.popup');
-let modalProfileName = modalWindow.querySelector('.popup__input_item_name');
-let modalProfileSpecial = modalWindow.querySelector('.popup__input_item_specialization');
-let profileName = document.querySelector('.profile__name');
-let profileSpecial = document.querySelector('.profile__specialization');
 
 function openModal() {
+  modalWindow.classList.add('popup_show');
+  popupSlice();
+}
+
+
+// --- ФУНКЦИЯ Загрузка формы профиля ---
+const formProfile = modalWindow.querySelector('.popup__profile');
+const modalProfileName = modalWindow.querySelector('.popup__input_item_name');
+const modalProfileSpecial = modalWindow.querySelector('.popup__input_item_specialization');
+const profileName = document.querySelector('.profile__name');
+const profileSpecial = document.querySelector('.profile__specialization');
+
+function loadProfile() {
+  closeForm();
+  //formNewCard.classList.remove('form_opened');
+  formProfile.classList.add('form_opened');
   modalProfileName.value = profileName.textContent;
   modalProfileSpecial.value = profileSpecial.textContent;
   modalProfileName.focus();
-  modalWindow.classList.add('popup_show');
-  popupSlice();
+  openModal();
+}
+
+// --- ФУНКЦИЯ Закрываю все открытые формы ---
+const forms = document.querySelectorAll('.form');
+
+const closeForm = () => {
+  forms.forEach((item) => {
+    item.classList.remove('form_opened');
+  });
+}
+
+
+
+// --- ФУНКЦИЯ Загрузка формы создания карты ---
+const formNewCard = modalWindow.querySelector('.popup__new-card');
+const modalCardName = modalWindow.querySelector('.popup__input_item_title');
+const modalCardPath = modalWindow.querySelector('.popup__input_item_path');
+
+function loadNewCard() {
+  closeForm();
+  //formProfile.classList.remove('form_opened');
+  formNewCard.classList.add('form_opened');
+  modalCardName.focus();
+  openModal();
 }
 
 // --- ФУНКЦИЯ Закрытие формы ---
@@ -18,6 +88,9 @@ function closeModal() {
   popupSlice()
   modalWindow.classList.remove('popup_show');
 }
+
+
+
 
 // --- ФУНКЦИЯ Отправка формы ---
 function formSubmitHandler(evt) {
@@ -27,12 +100,7 @@ function formSubmitHandler(evt) {
   closeModal();
 }
 
-// --- ФУНКЦИЯ плавного появления модального окна  ---
-let modalProfile = document.querySelector('.popup__body');
 
-function popupSlice() {
-  modalProfile.classList.toggle('popup__body_slice');
-}
 
 // --- Закрытие модального окна по ESC ---
 
@@ -52,10 +120,13 @@ function modalClickHandler(event) {
 }
 
 // ---БЛОК ОБРАБОТКИ СОБЫТИЙ ---
-let profileEditButton = document.querySelector('.profile__button-edit');
-profileEditButton.addEventListener('click', openModal);
+const profileEditButton = document.querySelector('.profile__button-edit');
+profileEditButton.addEventListener('click', loadProfile);
 
-let modalCloseButton = document.querySelector('.popup__button-close');
+const buttonCreateCard = document.querySelector('.profile__button-new-card');
+buttonCreateCard.addEventListener('click', loadNewCard);
+
+const modalCloseButton = document.querySelector('.popup__button-close');
 modalCloseButton.addEventListener('click', closeModal);
 
 modalProfile.addEventListener('submit', formSubmitHandler);
