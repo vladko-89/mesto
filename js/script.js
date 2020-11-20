8e42417cf36594865db88aa4f9fa3591237a7762
vladko-89 <63239096+vladko-89@users.noreply.github.com>
2020-11-20 11:31:58 +0400

    bugfix: Правки по ревью

--- js/script.js
+++ js/script.js
@@ -1,41 +1,40 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
+// --- БЛОК ГЛОБАЛЬНЫХ ПЕРЕМЕННЫХ ---
 const modalCardImage = document.querySelector('.popup__card-image');
 const modalSign = document.querySelector('.popup__sign');
 
+const showPopup = document.querySelector('.popup_show');
 
-// --- ФУНКЦИЯ плавного появления модального окна  ---
 const modalProfile = document.querySelector('.popup_type_profile');
 const modalNewCard = document.querySelector('.popup_type_new-card');
 const modalImage = document.querySelector('.popup_type_image');
 const modalBodyProfile = document.querySelector('.popup__body_modal_profile');
 const modalBodyNewCard = document.querySelector('.popup__body_modal_new-card');
 const modalBodyImage = document.querySelector('.popup__body_modal_image');
+
+const formProfile = modalProfile.querySelector('.popup__profile');
+const modalBodyProfileName = modalProfile.querySelector('.popup__input_item_name');
+const modalBodyProfileSpecial = modalProfile.querySelector('.popup__input_item_specialization');
+const profileName = document.querySelector('.profile__name');
+const profileSpecial = document.querySelector('.profile__specialization');
+
+const formNewCard = modalNewCard.querySelector('.popup__new-card');
+const modalCardName = modalNewCard.querySelector('.popup__input_item_title');
+const modalCardPath = modalNewCard.querySelector('.popup__input_item_path');
+
+const cardTitle = document.querySelector('.popup__input_item_title');
+const cardLink = document.querySelector('.popup__input_item_path');
+const cardsList = document.querySelector('.cards');
+
+const profileEditButton = document.querySelector('.profile__button-edit');
+const buttonCreateCard = document.querySelector('.profile__button-new-card');
+const modalCloseProfile = document.querySelector('.popup__button-close_modal_profile');
+const modalCloseNewCard = document.querySelector('.popup__button-close_modal_new-card');
+const modalCloseImage = document.querySelector('.popup__button-close_modal_image');
+// --- 
+
+
+// --- ФУНКЦИЯ плавного появления модального окна  ---
+
 
 const slidePopup = (body) => {
   body.classList.toggle('popup__body_slice');
@@ -54,6 +53,7 @@ function closeModal(popup, body) {
 const closeModalEvent = (evt) => {
   const showPopup = document.querySelector('.popup_show');
   const showBody = showPopup.querySelector('.popup__body');
+
   if (evt.key === 'Escape' || evt.target.classList.contains('popup_show')) {
     evt.preventDefault();
     closeModal(showPopup, showBody);
@@ -63,7 +63,7 @@ const closeModalEvent = (evt) => {
 // --- ФУНКЦИЯ Открытие формы ---
 
 const openModal = (popup, body) => {
-  blockSubmitButton(popup);
+
   popup.classList.add('popup_show');
   slidePopup(body);
   window.addEventListener('keydown', closeModalEvent);
@@ -72,32 +72,28 @@ const openModal = (popup, body) => {
 
 
 // --- ФУНКЦИЯ Загрузка формы профиля ---
-const formProfile = modalProfile.querySelector('.popup__profile');
-const modalBodyProfileName = modalProfile.querySelector('.popup__input_item_name');
-const modalBodyProfileSpecial = modalProfile.querySelector('.popup__input_item_specialization');
-const profileName = document.querySelector('.profile__name');
-const profileSpecial = document.querySelector('.profile__specialization');
-
-const loadProfile = () => {
+const openEditProfileModal = () => {
 
   modalBodyProfileName.value = profileName.textContent;
   modalBodyProfileSpecial.value = profileSpecial.textContent;
 
   modalBodyProfileName.focus();
+
+  blockSubmitButton(modalProfile);
+
   openModal(modalProfile, modalBodyProfile);
 };
 
 // --- ФУНКЦИЯ Загрузка формы создания карточки ---
-const formNewCard = modalNewCard.querySelector('.popup__new-card');
-const modalCardName = modalNewCard.querySelector('.popup__input_item_title');
-const modalCardPath = modalNewCard.querySelector('.popup__input_item_path');
-
-const loadNewCard = () => {
+const openAddCardModal = () => {
 
   modalCardName.value = '';
   modalCardPath.value = '';
 
   modalCardName.focus();
+
+  blockSubmitButton(modalNewCard);
+
   openModal(modalNewCard, modalBodyNewCard);
 };
 
@@ -113,10 +109,6 @@ const handleFormSubmit = (evt) => {
 };
 
 // --- ФУНКЦИЯ Генерация карточки ---
-const cardTitle = document.querySelector('.popup__input_item_title');
-const cardLink = document.querySelector('.popup__input_item_path');
-const cardsList = document.querySelector('.cards');
-
 const createCard = (cardData) => {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.cloneNode(true);
@@ -124,6 +116,7 @@ const createCard = (cardData) => {
 
   cardElement.querySelector('.card__title').textContent = cardData.name;
   cardImage.src = cardData.link;
+  cardImage.alt = `Карточка ${cardData.name}`;
 
   // СОБЫТИЕ ЛАЙК
   cardElement.querySelector('.card__like').addEventListener('click', function (evt) {
@@ -138,6 +131,7 @@ const createCard = (cardData) => {
   //СОБЫТИЕ ОТКРЫТИЕ БОЛЬШОЙ КАРТОЧКИ
   cardImage.addEventListener('click', () => {
     modalCardImage.src = cardData.link;
+    modalCardImage.alt = `Карточка ${cardData.name}`;
     modalSign.textContent = cardData.name;
 
     openModal(modalImage, modalBodyImage);
@@ -173,21 +167,20 @@ const loadCards = () => {
 loadCards();
 
 // ---БЛОК ОБРАБОТКИ СОБЫТИЙ ---
-const profileEditButton = document.querySelector('.profile__button-edit');
-profileEditButton.addEventListener('click', loadProfile);
 
-const buttonCreateCard = document.querySelector('.profile__button-new-card');
-buttonCreateCard.addEventListener('click', loadNewCard);
+profileEditButton.addEventListener('click', openEditProfileModal);
 
-const modalCloseProfile = document.querySelector('.popup__button-close_modal_profile');
+
+buttonCreateCard.addEventListener('click', openAddCardModal);
 modalCloseProfile.addEventListener('click', () => closeModal(modalProfile, modalBodyProfile));
 
-const modalCloseNewCard = document.querySelector('.popup__button-close_modal_new-card');
+
 modalCloseNewCard.addEventListener('click', () => closeModal(modalNewCard, modalBodyNewCard));
 
-const modalCloseImage = document.querySelector('.popup__button-close_modal_image');
+
 modalCloseImage.addEventListener('click', () => closeModal(modalImage, modalBodyImage));
 
 formProfile.addEventListener('submit', handleFormSubmit);
 
 formNewCard.addEventListener('submit', addCard);
+// ---
--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,6 @@
 //  --- БЛОК КОНФИГУРАЦИИ ---
 
-const config = {
+const validationConfig = {
   formSelector: '.form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button-submit',
@@ -57,10 +57,10 @@ const toggleButtonState = (inputList, buttonItem, config) => {
 
 // --- ФУНКЦИЯ Блокируем кнопку отправки формы ---
 const blockSubmitButton = (popup) => {
-  const buttonSubmit = popup.querySelector(config.submitButtonSelector);
+  const buttonSubmit = popup.querySelector(validationConfig.submitButtonSelector);
 
   if (buttonSubmit) {
-    buttonSubmit.classList.add(config.inactiveButtonClass);
+    buttonSubmit.classList.add(validationConfig.inactiveButtonClass);
   }
 };
 
@@ -95,86 +95,4 @@ const enableValidation = (config) => {
   });
 };
 
-enableValidation(config);
-
-
-
-/*
-// --- ФУНКЦИЯ Показать ошибку ввода---
-const showInputError = (formItem, inputItem, errorMessage) => {
-  const errorItem = formItem.querySelector(`#${inputItem.id}-error`);
-
-  inputItem.classList.add('popup__input_type_error');
-  errorItem.textContent = errorMessage;
-  errorItem.classList.add('popup__error_active');
-};
-
-// --- ФУНКЦИЯ Скрыть ошибку ввода---
-const hideInputError = (formItem, inputItem) => {
-  const errorItem = formItem.querySelector(`#${inputItem.id}-error`);
-
-  inputItem.classList.remove('popup__input_type_error');
-  errorItem.classList.remove('popup__error_active');
-  errorItem.textContent = '';
-};
-
-// --- ФУНКЦИЯ Валидация поля ввода формы ---
-const isValid = (formItem, inputItem) => {
-  if (!inputItem.validity.valid) {
-    showInputError(formItem, inputItem, inputItem.validationMessage);
-  }
-  else {
-    hideInputError(formItem, inputItem);
-  }
-};
-
-// ---- ФУНКЦИЯ Проверяем валидность всех полей текущей формы ---
-const hasValidInput = (inputList) => {
-  return inputList.some((inputItem) => {
-
-    return !inputItem.validity.valid;
-  });
-};
-
-// --- ФУНКЦИЯ Управляем доступностью кнопки отправки текущей формы ---
-const toggleButtonState = (inputList, buttonItem) => {
-  if (hasValidInput(inputList)) {
-    buttonItem.classList.add('popup__button-submit_disabled');
-  }
-  else {
-    buttonItem.classList.remove('popup__button-submit_disabled');
-  }
-};
-
-// --- ФУНКЦИЯ Подключение обработчиков событий ко всем полям формы ---
-const setEventListeners = (formItem) => {
-  const inputList = Array.from(formItem.querySelectorAll('.popup__input'));
-  const buttonItem = formItem.querySelector('.popup__button-submit');
-
-  toggleButtonState(inputList, buttonItem);
-
-  inputList.forEach((inputItem) => {
-    inputItem.addEventListener('input', () => {
-
-      isValid(formItem, inputItem);
-
-      toggleButtonState(inputList, buttonItem);
-    });
-  });
-};
-
-
-// --- ФУНКЦИЯ Подключаем обработчики событий ко всем формам ---
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.form'));
-
-  formList.forEach((formItem) => {
-    formItem.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    });
-
-    setEventListeners(formItem);
-  });
-};
-
-enableValidation(); */
+enableValidation(validationConfig);
